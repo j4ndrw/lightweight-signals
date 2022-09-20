8d343aa1957a21c2ca298fb0c2d76f312f413303
j4ndrw <murjan_andreialin@yahoo.com>
2022-09-20 10:32:15 +0300

    chore: More pre-publish stuff

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface Signal<T> {
-    value: T,
-    set: (newValue: T) => void,
-    setFrom: (setter: (prevValue: T) => T) => void;
-}
-
-declare interface Computed<T> {
-    value: T
-}
